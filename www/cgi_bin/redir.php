15cfbe553adb9afd9528d1f9b374f96f049afc0b
brahim elkartouch <belkarto@student.1337.ma>
2024-01-17 01:06:49 +0100

    midifier

---
+++ www/cgi_bin/redir.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php-cgi
+<?php
+// PHP program to describes header function
+
+// Redirect the browser
+header("Location: https://www.geeksforgeeks.org");
+
+// The below code does not get executed 
+// while redirecting
+exit;
+?>
+
